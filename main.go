9b7e76c15728cb7f5c23a666dd914823f92542f5
carfogoh <50168897+carfogoh@users.noreply.github.com>
2026-02-14 22:16:17 +0300

    .

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
+// Declare the package name. "main" is required for an executable program
 package main
+// Import the "fmt" package for formatted I/O (input/output)
 import "fmt"
-  func main() {
-      fmt.Println("Hello, Go! \nThat was an awesome test")
-  }
+// Entry point of the program
+func main() {
+    // Print text to the console with a newline in between
+    fmt.Println("Hello, Go!")
+}
